fad9644baf4cac0a60b339ebe286c8c3a18ea989
Graham <gmcallister@gatech.edu>
2018-02-28 23:03:14 -0500

    removed sensitive config

--- src/environment.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDPr_smunA_RpcWByjClYrTcWdDJyb0--M',
-  authDomain: 'jacket-tracker-90b5c.firebaseapp.com',
-  databaseURL: 'https://jacket-tracker-90b5c.firebaseio.com',
-  projectId: 'jacket-tracker-90b5c',
-  storageBucket: 'jacket-tracker-90b5c.appspot.com',
-  messagingSenderId: '674608756377'
-};
