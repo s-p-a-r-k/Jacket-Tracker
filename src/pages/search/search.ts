a22141b05fbc31c4d47a56e626bcf24f45d82da0
scho300 <bcho1996@gmail.com>
2018-03-09 02:53:03 -0500

    Create list of searched

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { LoggedinPage } from '../loggedin/loggedin';
 import { QuickmanagementPage } from '../quickmanagement/quickmanagement';
 import { AngularFireDatabase, DatabaseSnapshot } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 
 /**
@@ -19,19 +20,33 @@ import { AngularFireDatabase, DatabaseSnapshot } from 'angularfire2/database';
 })
 export class SearchPage {
   searchLocation: any;
+
   type: any;
   uniformID: any;
   other: any;
+
   firstName: any;
   lastName: any;
   gtid: any;
   email: any;
   section: any;
 
+  equipRecord: Observable<any[]>;
+  studentRecord: Observable<any[]>;
+  equiplist;
+  studentlist;
+
+  templist = [];
+  matchlist = [];
   
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afDB: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
     this.searchLocation = "student";
+    this.studentRecord = afDB.list('students').valueChanges();
+    this.studentRecord.subscribe(_afDB => {this.studentlist = _afDB})
+    this.equipRecord = afDB.list('equipment').valueChanges();
+    this.equipRecord.subscribe(_afDB => {this.equiplist = _afDB})
+
   }
 
   ionViewDidLoad() {
@@ -42,18 +57,93 @@ export class SearchPage {
   backToHome() {
     this.navCtrl.setRoot(LoggedinPage);
   }
+  matchFirstName() {
 
+  }
   searchClicked() {
-    this.navCtrl.setRoot(QuickmanagementPage);
 
-    console.log(this.type);
-    console.log(this.uniformID);
-    console.log(this.other);
-    console.log(this.firstName);
-    console.log(this.lastName);
-    console.log(this.gtid);
-    console.log(this.email);
-    console.log(this.section);
+    //Lieutenant searches through student tab
+    if(this.searchLocation == "student") {
+      if(this.firstName != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.firstname.toLowerCase() == this.firstName.toLowerCase();
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+
+      if(this.lastName != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.lastname.toLowerCase() == this.lastName.toLowerCase();
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }      
+
+      if(this.gtid != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.gtid == this.gtid;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+
+      if(this.email != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.email == this.email;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+
+      if(this.email != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.email == this.email;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+      /* Commented out until storing section is implemented when checking out
+      if(this.section != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.section == this.section;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }*/
+      /* Commented out until storing other is implemented
+      if(this.other != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.other == this.other;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }*/
+    }
+    //Lieutenant searches through uniform tab
+    if(this.searchLocation == "uniform") {
+
+      if(this.type != null) {
+        this.templist = this.equiplist.filter((item)=> {
+          return item == this.type;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+
+      if(this.uniformID != null) {
+        this.templist = this.equiplist.filter((item)=> {
+          return item.uniformID == this.uniformID;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }
+      /* Commented out until storing other is implemented
+      if(this.other != null) {
+        this.templist = this.studentlist.filter((item)=> {
+          return item.other == this.other;
+        });
+        this.templist.forEach(item=> this.matchlist.push(item));
+      }*/
+    }
+    //delete duplicates
+    this.matchlist = this.matchlist.reduce((x,y) => x.findIndex(e => e.email==y.email) < 0 ? [...x, y]: x, []);
+    console.log(this.equiplist);
+    console.log(this.studentlist);
+    console.log(this.matchlist);
+
+    
 
   }
 
