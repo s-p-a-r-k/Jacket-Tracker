dc54eb2e0837385ded4691ca194cd4c4ac8c7acb
spark638 <spark638@gatech.edu>
2018-03-20 22:38:08 -0400

    email working

--- src/pages/quickmanagement/quickmanagement.ts
+++ src/pages/quickmanagement/quickmanagement.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams , AlertController} from 'ionic-angular';
 
+import { HttpClient } from '@angular/common/http';
+
 import { AngularFireDatabase, DatabaseSnapshot } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
@@ -31,11 +33,19 @@ export class QuickmanagementPage {
   equipRecordRef;
   studentRecordRef;
 
-  constructor(public navCtrl: NavController, private afDB: AngularFireDatabase, public alertCtrl: AlertController, private afAuth: AngularFireAuth, public events: Events) {
+  http: HttpClient;
+  mailgunUrl: string;
+  mailgunApiKey: string;
+
+  constructor(public navCtrl: NavController, private afDB: AngularFireDatabase, public alertCtrl: AlertController, private afAuth: AngularFireAuth, public events: Events, http: HttpClient) {
     this.items = afDB.list('students').valueChanges();
     this.items.subscribe(_afDB => {this.itemarr = _afDB})
     this.equipRecordRef = this.afDB.list('equipment');
     this.studentRecordRef = this.afDB.list('students');
+
+    this.http = http;
+    this.mailgunUrl = "sandboxSeeSlackForCorrectAddress.mailgun.org";
+    this.mailgunApiKey = "key-SeeSlcakForCorrectKey";
 
     console.log('========================================');
     console.log(this.items);
@@ -67,6 +77,7 @@ export class QuickmanagementPage {
   quickManageSubmit(selectedAction) {
       if (selectedAction == "email") {
         console.log('email option selected');
+        this.send();
       } else if (selectedAction == "uniform"){
         console.log('uniform status option selected');
         this.doRadio();
@@ -82,6 +93,21 @@ export class QuickmanagementPage {
 
   updateStatus(result, student, equiptype) {
     this.arrChosen[student].equipment[equiptype].status = this.radioResult;
+  }
+
+  send() {
+
+      this.http.request(
+        "POST", "https://api.mailgun.net/v3/" + this.mailgunUrl + "/messages",
+        {
+          body: "from=sender@example.com&to=" + "recipient@example.com" + "&subject=" + "Test Email" + "&text=" + "Example message sent.",
+          headers: {"Authorization": "Basic " + window.btoa("api:" + this.mailgunApiKey),
+          "Content-Type": "application/x-www-form-urlencoded"}
+        }).subscribe(success => {
+        console.log("SUCCESS -> " + JSON.stringify(success));
+      }, error => {
+        console.log("ERROR -> " + JSON.stringify(error));
+      });
   }
 
   doRadio() {
